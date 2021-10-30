fc3d8aab0482b388f3450e35dfd9b91f4f7424fe
kesselyn <kesselynfortunato@hotmail.com>
2021-10-29 21:21:10 -0300

    botoes funcionais

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-let entrada = "55C"
-let palavra = ""
-let fields;
-let total = 0
+let entrada = "";
+let palavra = "";
+let total = 0;
+let buttonsActive = [];
 
-function total (entrada) {
-    if (!isNaN(entrada))
-        total += entrada
-    return total
-}
-
-function enableField(total) {
-    switch (total) {
-        case total = 6:
-            return ["A"];
-        case total = 7:
-            return ["A", "B"];
-        case total >=8 :
-            return ["A", "B", "C"];
-        default:
-            return [];
+function enableButton(total) {
+    if (total >= 6 && !buttonsActive.find(e => e == "A")) {
+        document.getElementById("btnA").disabled = false;
+        buttonsActive.push("A");
+    } 
+    if (total >= 7 && !buttonsActive.find(e => e == "B")) {
+        document.getElementById("btnB").disabled = false;
+        buttonsActive.push("B");
+    }
+    if (total >= 8 && !buttonsActive.find(e => e == "C")) {
+        document.getElementById("btnC").disabled = false;
+        buttonsActive.push("C");
     }
 }
 
-function transicao () {
+function transicao() {
 
 }
 
-function main (entrada) {
+function main(entrada) {
     palavra += entrada;
-    total (entrada);
-    //tratar fields como conjunto
-    fields = enableField(total);
-    result = transicao(total)
-    return result
+    if (!isNaN(entrada)) {
+        total += entrada;
+        total > 5 && enableButton(total);
+        document.getElementById("total").innerHTML = `TOTAL: R$ ${total},00`;
+    }
+    else {
+        document.getElementById("btnA").disabled = true;
+        document.getElementById("btnB").disabled = true;
+        document.getElementById("btnC").disabled = true;
+        // transicao(palavra)
+    }
 }
