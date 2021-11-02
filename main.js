2a09ebb307936fe5afa69ee87309926c459ab20a
Davi Fonseca <davi.brandao2013@gmail.com>
2021-11-02 02:31:08 -0300

    Funciona, só não implementei AFD

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let entrada = "";
 let palavra = "";
 let total = 0;
+let troco = 0;
 let buttonsActive = [];
 
 function enableButton(total) {
@@ -18,9 +19,35 @@ function enableButton(total) {
     }
 }
 
-function transicao() {
-
+function disableButtons(){
+    document.getElementById("btnA").disabled = true;
+    document.getElementById("btnB").disabled = true;
+    document.getElementById("btnC").disabled = true;
+    // bloqueia money
+    document.getElementById("cash1").disabled = true;
+    document.getElementById("cash2").disabled = true;
+    document.getElementById("cash5").disabled = true;
 }
+
+function transicao(palavra, entrada) {
+    switch (entrada){
+        case'A':
+            troco = total - 6;
+            break;
+        case'B':
+            troco = total - 7;
+             break;
+        case'C':
+            troco = total - 8;
+            break;
+        default:
+            console.log('err!')
+        }
+    document.getElementById("troco").innerHTML = `TROCO: R$ ${troco},00`;
+    document.getElementById("troco").style.backgroundColor = `rgb(152, 243, 116)`;
+    document.getElementById("candy").innerHTML = `${entrada}`;
+    document.getElementById("candy").style.backgroundColor = `rgb(152, 243, 116)`;
+    }
 
 function main(entrada) {
     palavra += entrada;
@@ -28,11 +55,8 @@ function main(entrada) {
         total += entrada;
         total > 5 && enableButton(total);
         document.getElementById("total").innerHTML = `TOTAL: R$ ${total},00`;
-    }
-    else {
-        document.getElementById("btnA").disabled = true;
-        document.getElementById("btnB").disabled = true;
-        document.getElementById("btnC").disabled = true;
-        // transicao(palavra)
+    } else {
+        disableButtons();
+        transicao(palavra, entrada);
     }
 }
